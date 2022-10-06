d5f35e210fba2d1ddc5e59a146bfd4bd1b1032a8
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2022-10-06 09:32:36 -0400

    add raycast

--- Assets/TestRaycast.cs
+++ Assets/TestRaycast.cs
@@ -7,10 +7,13 @@ public class TestRaycast : MonoBehaviour
     public Transform cible;
     private SpriteRenderer rendu;
     public float maxDistance = 5.0f;
+
+    public LayerMask masqueRayon; // pour interagir sur certain layer
+
     // Start is called before the first frame update
     void Start()
     {
-        rendu = GetComponent<SpriteRenderer>();
+        rendu = GetComponent<SpriteRenderer>(); // prend le premier des renderers
     }
 
     // Update is called once per frame
@@ -18,6 +21,29 @@ public class TestRaycast : MonoBehaviour
     {
         Vector3 rayon = cible.position - transform.position;
         rayon.Normalize();
-        Debug.DrawRay(transform.position, rayon * maxDistance);
+
+
+         
+
+        Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y), rayon, maxDistance); // vecteur 2D
+        // possible de juste utiliser transform.position, il va ignorer le Z
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, rayon, maxDistance, masqueRayon);
+
+        if (hit.collider != null) // pour retirer les contacts avec environements
+        {
+            Debug.DrawLine(transform.position, hit.point);
+            rendu.color = Color.blue;
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Acteur"))
+            {
+                rendu.color = Color.red;
+
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, rayon * maxDistance);
+            rendu.color = Color.blue;
+        }
     }
 }
